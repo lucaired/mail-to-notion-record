e98bc18323db6af3efbc6cba466486a274d4f912
lucaired <lucaired@users.noreply.github.com>
2024-02-11 21:56:47 +0100

    Define architecture

---
+++ src/envLoader.ts
@@ -0,0 +1,37 @@
+interface NotionConfiguratation {
+  notionToken: string;
+  notionDatabaseId: string;
+}
+
+interface SMPTConfiguration {
+  host: string;
+  port: number;
+  user: string;
+  pass: string;
+}
+
+export interface Configuration
+  extends NotionConfiguratation,
+    SMPTConfiguration {}
+
+export const loadEnv = (): Configuration => {
+  const notionToken = process.env.NOTION_TOKEN;
+  const notionDatabaseId = process.env.NOTION_DATABASE_ID;
+  const host = process.env.SMTP_HOST;
+  const port = process.env.SMTP_PORT;
+  const user = process.env.SMTP_USER;
+  const pass = process.env.SMTP_PASS;
+
+  if (!notionToken || !notionDatabaseId || !host || !port || !user || !pass) {
+    throw new Error("Invalid configuration");
+  }
+
+  return {
+    notionToken,
+    notionDatabaseId,
+    host,
+    port: parseInt(port),
+    user,
+    pass,
+  };
+};
